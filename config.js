c4e2720119425971b09ab817d2d482a8133954db
cybershield135 <hytesting56@gmail.com>
2026-06-20 08:16:35 +0700

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-const CONFIG = {
-    GEMINI_API_KEY: 'AQ.Ab8RN6Lwsl...', 
-    // Sửa 'v1' thành 'v1beta'
-    API_URL: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent'
-};
